c0d1bc3fdaf35e45cceeb5f27b26536edd4c4d7b
Jayam Malviya <malviya.jayam@gmail.com>
2018-03-15 15:18:14 -0700

    aut

---
+++ zzSVD/myNNF.py
@@ -0,0 +1,116 @@
+import data
+import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+matrix = data.read_and_create_user_Matrix()
+
+num_input = 16981
+num_hidden_1 = 10
+num_hidden_2 = 5
+
+X = tf.placeholder(tf.float64, [None, num_input])
+
+weights = {
+    'encoder_h1': tf.Variable(tf.random_normal([num_input, num_hidden_1], dtype=tf.float64)),
+    'encoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_hidden_2], dtype=tf.float64)),
+    'decoder_h1': tf.Variable(tf.random_normal([num_hidden_2, num_hidden_1], dtype=tf.float64)),
+    'decoder_h2': tf.Variable(tf.random_normal([num_hidden_1, num_input], dtype=tf.float64)),
+}
+
+biases = {
+    'encoder_b1': tf.Variable(tf.random_normal([num_hidden_1], dtype=tf.float64)),
+    'encoder_b2': tf.Variable(tf.random_normal([num_hidden_2], dtype=tf.float64)),
+    'decoder_b1': tf.Variable(tf.random_normal([num_hidden_1], dtype=tf.float64)),
+    'decoder_b2': tf.Variable(tf.random_normal([num_input], dtype=tf.float64)),
+}
+
+# Building the encoder
+
+def encoder(x):
+    # Encoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['encoder_h1']), biases['encoder_b1']))
+    # Encoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['encoder_h2']), biases['encoder_b2']))
+    return layer_2
+
+
+# Building the decoder
+
+def decoder(x):
+    # Decoder Hidden layer with sigmoid activation #1
+    layer_1 = tf.nn.sigmoid(tf.add(tf.matmul(x, weights['decoder_h1']), biases['decoder_b1']))
+    # Decoder Hidden layer with sigmoid activation #2
+    layer_2 = tf.nn.sigmoid(tf.add(tf.matmul(layer_1, weights['decoder_h2']), biases['decoder_b2']))
+    return layer_2
+
+
+# Construct model
+
+encoder_op = encoder(X)
+decoder_op = decoder(encoder_op)
+
+
+# Prediction
+
+y_pred = decoder_op
+
+
+# Targets are the input data.
+
+y_true = X
+
+
+# Define loss and optimizer, minimize the squared error
+
+loss = tf.losses.mean_squared_error(y_true, y_pred)
+optimizer = tf.train.RMSPropOptimizer(0.03).minimize(loss)
+
+predictions = pd.DataFrame()
+
+# Define evaluation metrics
+
+eval_x = tf.placeholder(tf.int32, )
+eval_y = tf.placeholder(tf.int32, )
+pre, pre_op = tf.metrics.precision(labels=eval_x, predictions=eval_y)
+
+# Initialize the variables (i.e. assign their default value)
+
+init = tf.global_variables_initializer()
+local_init = tf.local_variables_initializer()
+
+
+
+with tf.Session() as session:
+    epochs = 50
+    batch_size = 250
+
+    session.run(init)
+    session.run(local_init)
+
+    num_batches = int(matrix.shape[0] / batch_size)
+    matrix = np.array_split(matrix, batch_size)
+
+    for i in range(epochs):
+
+        avg_cost = 0
+
+        for batch in matrix:
+            _, l = session.run([optimizer, loss], feed_dict={X: batch})
+            avg_cost += l
+
+
+        avg_cost /= num_batches
+
+        print("Epoch: {} Loss: {}".format(i + 1, avg_cost))
+
+    print("Predictions...")
+
+    matrix = np.concatenate(matrix, axis=0)
+
+    preds = session.run(decoder_op, feed_dict={X: matrix})
+
+    predictions = predictions.append(pd.DataFrame(preds))
+
+    print(predictions)
+    predictions.to_csv('out.csv')
