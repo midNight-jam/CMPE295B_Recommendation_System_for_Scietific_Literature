266ea09c2a9dc92cd60f1c7d53896a4e5e4ab21d
Jayam Malviya <malviya.jayam@gmail.com>
2018-05-03 04:55:28 -0700

    content_ml 2

--- neural_network_exp/content_ml.py
+++ neural_network_exp/content_ml.py
@@ -17,30 +17,39 @@ print('Cosine Similarites calculated')
 print(cosine_similarities.shape)
 results = {}
 
-for idx in range(0, 4000):
-	similar_indices = cosine_similarities[idx].argsort()[:-50:-1]
-	similar_items = [(cosine_similarities[idx][i], i) for i in similar_indices]
+for idx in range(0, 16980):
+  similar_indices = cosine_similarities[idx].argsort()[:-50:-1]
+  similar_items = [(cosine_similarities[idx][i], i) for i in similar_indices]
 
 	# First item is the item itself, so remove it.
 	# Each dictionary entry is like: [(1,2), (3,4)], with each tuple being (score, item_id)
-	results[idx] = similar_items[1:]
+  results[idx] = similar_items[1:]
+
 print('Done calculating the similar ones')
 print('----'*8)
 print(results[0])
 print('----'*8)
-# def recommend():
+
+content_ml_rec_details_output_file = open(config.content_ml_rec_details_output_file, "w")
 content_ml_rec_output_file = open(config.content_ml_rec_output_file, "w")
 print("-------"*5)
-for i in range(0, 4000):
-	item_id = i
-	recs = results[item_id][:config.top_k_products]
-	user_rec = []
-	for rec in recs:
-		user_rec.append(rec[1],rec[0])
-	print('u : {0}   rec_papers {1}\n'.format(item_id, str(user_rec)))
-	content_ml_rec_output_file.write('u : {0}   rec_papers {1}\n'.format(item_id, str(user_rec)))
+
+for i in range(0, 16980):
+  item_id = i
+  recs = results[item_id][:config.top_k_products]
+  user_rec = []
+  user_rec_papers = []
+  for rec in recs:
+    user_rec.append((rec[1],rec[0]))
+    user_rec_papers.append(rec[1])
+  print("u : {0}   rec_papers {1}".format(item_id, str(user_rec_papers)))
+  content_ml_rec_details_output_file.write("u : {0}   rec_papers {1}\n".format(item_id, str(user_rec)))
+  docs = " ".join(str(d) for d in user_rec_papers)
+  content_ml_rec_output_file.write("{0} {1}\n".format(len(user_rec_papers), docs))
+
+content_ml_rec_details_output_file.close()
 content_ml_rec_output_file.close()
 
 ######################################################
-process()
+
 ######################################################
