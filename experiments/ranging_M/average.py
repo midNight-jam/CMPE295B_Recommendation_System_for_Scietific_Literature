77411f57d2987efb8e9d0ce09ecbe30fc9121b2d
Jayam Malviya <malviya.jayam@gmail.com>
2018-05-09 18:49:40 -0700

    ranging_M

--- experiments/ranging_M/average.py
+++ experiments/ranging_M/average.py
@@ -1,15 +1,28 @@
 def average():
-  sum = 0.0
+  sum1 = 0.0
+  sum2 = 0.0
   total = 5551.0
+  i = 1 
+  count1 = 1834.0
+  count2 = 3717.0
+
   f = open(file)
   for line in f:
     val = float(line)
-    sum += val
+
+    if(i >= count1):
+      sum2 += val
+    else:
+      sum1 += val
+    i += 1
   f.close()
-  avg = (sum / total)
-  print('Average : {0}'.format(avg))
 
+  avg1 = (sum1 / count1)
+  avg2 = (sum2 / count2)
 
-file = 'auto_enco_svd/auto_enco_svd_recall___M_@_10___readings__2018_05_09T01_35_59_028229.dat'
+  print('Average1 : {0} = {1} / {2}'.format(avg1, sum1, count1))
+  print('Average2 : {0} = {1} / {2}'.format(avg2, sum2, count2))
+
+file = 'cdl/recall___M_@_10___readings__2018_05_09T13_59_06_811622.dat'
 print(file)
 average()
